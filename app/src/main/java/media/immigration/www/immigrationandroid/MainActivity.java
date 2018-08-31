9b1bf7350ef232a3b60a62f2b18de73d0286461a
Jakub Flaska <jakub.flaska@nih.gov>
2018-08-31 19:45:21 -0400

    x

--- app/src/main/java/media/immigration/www/immigrationandroid/MainActivity.java
+++ app/src/main/java/media/immigration/www/immigrationandroid/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         WebView webView = (WebView) findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl("https://www.immigration.media/");
     }
 }
