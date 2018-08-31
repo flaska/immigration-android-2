c5e93420e76a2f3cf2844d50b063429f650a8308
Jakub Flaska <jakub.flaska@nih.gov>
2018-08-31 17:16:46 -0400

    x

--- app/src/main/java/media/immigration/www/immigrationandroid/MainActivity.java
+++ app/src/main/java/media/immigration/www/immigrationandroid/MainActivity.java
@@ -2,6 +2,7 @@ package media.immigration.www.immigrationandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+        webView.loadUrl("https://www.immigration.media/");
     }
 }
